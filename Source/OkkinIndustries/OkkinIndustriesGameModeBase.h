2ae5711da5027c9cc3026adddace8ad379abe66d
katie-boyd <katie.simmons987@gmail.com>
2021-10-12 20:43:49 -0400

    Initial commit

---
+++ Source/OkkinIndustries/OkkinIndustriesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OkkinIndustriesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OKKININDUSTRIES_API AOkkinIndustriesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
