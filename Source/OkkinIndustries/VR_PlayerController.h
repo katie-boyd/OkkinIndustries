ff7951aabeacb3b05b326741e14c88b4e5b166bf
katie-boyd <katie.simmons987@gmail.com>
2021-10-30 15:29:22 -0400

    Input ActionMappings & Created the stubs for some classes

---
+++ Source/OkkinIndustries/VR_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRPlayerController.h"
+#include "VR_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OKKININDUSTRIES_API AVR_PlayerController : public AVRPlayerController
+{
+	GENERATED_BODY()
+	
+};
