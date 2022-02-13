8e95d7e70080c8cc661e4d8b679b74e31ced8ed8
katie-boyd <katie.simmons987@gmail.com>
2022-02-13 15:36:42 -0500

    Updates for grippable component. Add VR Player Controller

---
+++ Source/OkkinIndustries/Public/OI_VRPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRPlayerController.h"
+#include "OI_VRPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OKKININDUSTRIES_API AOI_VRPlayerController : public AVRPlayerController
+{
+	GENERATED_BODY()
+	
+};
